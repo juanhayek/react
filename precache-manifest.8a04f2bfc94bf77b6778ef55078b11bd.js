30b4ecfd2b1da0368c7627f8df42371c2e432e73
juanhayek <hayek17@gmail.com>
2020-07-06 15:27:03 -0500

    Updates

---
+++ precache-manifest.8a04f2bfc94bf77b6778ef55078b11bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0feae53655ce54ea3daea92ede7aaabb",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "5f2fe97278d832a0ee99",
+    "url": "/react/static/css/main.8d917b55.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2fe97278d832a0ee99",
+    "url": "/react/static/js/main.b40113e2.chunk.js"
+  },
+  {
+    "revision": "e16830573a61ddcfcf9f",
+    "url": "/react/static/js/runtime-main.54f9499d.js"
+  }
+]);
